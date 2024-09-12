43854699745003d73b480918eee06fff1437dce5
Nigoritsa-Nikita <nigarikns713@gmail.com>
2024-09-12 14:51:20 +0300

    8 + 4 + " d" 12 d

---
+++ JobCandidate/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace JobCandidate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(8 + 4 + " d");
+            Console.ReadLine();
+        }
+    }
+}
